f9106731ad9e212853c1e5928648e03fcbcc38e0
borischen425 <96858963+borischen425@users.noreply.github.com>
2022-12-29 17:39:46 +0800

    Update eyemovement_realtime_data.py

--- eyemovement_realtime_data.py
+++ eyemovement_realtime_data.py
@@ -45,18 +45,13 @@ a = []
 b = 1
 
 # Host machine IP
-HOST = '34.127.33.101'
+HOST = '140.138.83.101'
 # Gazepoint Port
 PORT = 4242
 ADDRESS = (HOST, PORT)
-timeout = 60 * 1
 
 # Connect to Gazepoint API
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(10)
-s.connect(ADDRESS)
-
-s.settimeout(None)
 s.connect(ADDRESS)
 
 # Send commands to initialize data streaming
